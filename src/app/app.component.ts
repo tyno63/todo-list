0938b87e9705bac6641815993cb400353b2d61b5
tyno63 <antoninovichry@yahoo.fr>
2021-01-20 16:08:20 +0100

    new item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  public todoArray: Array<string> = [
+    `item 1`,
+    `item 2`,
+    `item 3`,
+    `item 4`,
+    `item 5`,
+    `pouloulou`,
+  ];
 }
