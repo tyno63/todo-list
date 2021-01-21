610e91d4bcc4df147fb2211a96cf11cd6543f345
tyno63 <antoninovichry@yahoo.fr>
2021-01-21 09:18:17 +0100

    amélio js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,29 @@ export class AppComponent {
   title = 'todo-list';
 
   public todoArray: Array<string> = [
-    `item 1`,
-    `item 2`,
-    `item 3`,
-    `item 4`,
-    `item 5`,
-    `pouloulou`,
+    'item 1',
+    'item 2',
+    'item 3',
+    'item 4',
+    'item 5',
+    'foobar',
   ];
 
+  /**
+   * This method is used in app.component.html on a keyup event on the text input.
+   * It take a KeyboardEvent as argument and return void (mean its return nothing).
+   */
   public addItem($event: KeyboardEvent): void {
-   if ($event.code === `enter`) {
-     const $input: HTMLInputElement|null = $event.target as HTMLInputElement;
+    // Check if keyboard key pressed is Enter.
+    if ($event.code === 'Enter') {
+      // Get the event source html element. In our case, its an input element.
+      const $input: HTMLInputElement|null = $event.target as HTMLInputElement;
 
-     if ($input !== null) {
-       console.log($input.value);
-     }
-    console.log(`enter pressed`);
-    } 
+      if ($input !== null) {
+        console.log($input.value);
+      }
+
+      console.log('Enter pressed');
+    }
   }
 }
